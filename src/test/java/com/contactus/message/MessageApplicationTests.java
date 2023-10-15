e7ac1f625f722f7a6cd8caf51f6fa37493837413
rejikrish83 <rejikrish83@gmail.com>
2023-10-15 21:50:19 +0100

    Mocking test

--- src/test/java/com/contactus/message/MessageApplicationTests.java
+++ src/test/java/com/contactus/message/MessageApplicationTests.java
@@ -1,6 +1,7 @@
 package com.contactus.message;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,10 +11,13 @@ import com.amazonaws.services.ec2.AmazonEC2;
 
 
 class MessageApplicationTests {
-
+	@Mock
+    private AmazonEC2 ec2Client;
+	
 	@Test
 	void contextLoads() {
 		 AmazonEC2 ec2Client = Mockito.mock(AmazonEC2.class);
+		
 	}
 	
 	
