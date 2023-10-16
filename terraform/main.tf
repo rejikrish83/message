ad64916000e9a57766b9c6b8a24036e1cf205049
rejikrish83 <rejikrish83@gmail.com>
2023-10-16 22:42:20 +0100

    updated Terrraform script to deploy the app in ECS

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,7 +55,7 @@ resource "aws_ecs_task_definition" "messageapp" {
     {
       "name": "messageapp-container",
       "image": "470502905291.dkr.ecr.eu-north-1.amazonaws.com/message-app:latest",
-      "memory": 1024
+      "memory": 1024,
       "cpu": 512,
       "essential": true,
       "portMappings": [
