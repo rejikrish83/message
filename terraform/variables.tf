e5182dda1ce356c50db0c5ec0f783701539343bc
rejikrish83 <rejikrish83@gmail.com>
2023-10-16 18:33:21 +0100

    updated Terrraform script to deploy the app in ECS

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,3 @@
-variable "account_id" {
-  type        = string
-  description = "AWS Account ID"
-}
-
-variable "cross_account_accountid" {
-  type        = string
-  description = "AWS Account ID"
-}
 
 variable "region" {
   type    = string
