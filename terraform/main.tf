f11c446d695b396dbb7b47312cfe0db6925f026b
rejikrish83 <rejikrish83@gmail.com>
2023-10-16 23:47:20 +0100

    updated Terrraform script to deploy the app in ECS

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,7 +88,7 @@ resource "aws_iam_role" "messageapp" {
     Version = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole",
+        
         Effect = "Allow",
         Principal = {
           Service = "ecs-tasks.amazonaws.com"
