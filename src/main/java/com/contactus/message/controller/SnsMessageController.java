7c80c787209d081c9a67be6cc590237f2a3c24b9
rejikrish83 <rejikrish83@gmail.com>
2023-10-17 16:24:08 +0100

    updated ECR image path

--- src/main/java/com/contactus/message/controller/SnsMessageController.java
+++ src/main/java/com/contactus/message/controller/SnsMessageController.java
@@ -2,6 +2,8 @@ package com.contactus.message.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +28,13 @@ public class SnsMessageController {
     @PostMapping("/publish")
     public void publishMessage(@RequestBody String message) {
         snsService.publishMessageToTopic(ssmPara.getSSMParameter(contactustopicArn), message);
+    }
+    
+    @GetMapping("/")
+    public ResponseEntity<String> getExampleData() {
+        String data = "This is an example response.";
+        
+        return ResponseEntity.ok("Success...");
     }
 }
 
