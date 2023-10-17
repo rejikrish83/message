9e18036edc45bf689e11485feef276150231f186
rejikrish83 <rejikrish83@gmail.com>
2023-10-17 03:26:36 +0100

    updated Terrraform script to deploy the app in ECS

--- terraform/SNSTopic.tf
+++ terraform/SNSTopic.tf
@@ -7,9 +7,11 @@ resource "aws_sns_topic" "SNSTopic" {
 
 # Subscribe an email address to the SNS topic
 resource "aws_sns_topic_subscription" "email_subscription" {
+  count      = length(var.email_addresses)
   topic_arn = aws_sns_topic.SNSTopic.arn
   protocol  = "email"
-  endpoint  = "dainius.stepulevicius@t4connex.com,benjamin.wai@t4connex.com"
+  endpoint   = element(var.email_addresses, count.index)
+  
 }
 
 # Store the SNS topic ARN in SSM Parameter Store
@@ -17,6 +19,7 @@ resource "aws_ssm_parameter" "sns_topic_arn" {
   name  = "/config/message_develop/contactustopic-arn"
   type  = "String"
   value = aws_sns_topic.SNSTopic.arn
+  overwrite = true
 }
 
 output "sns_topic_arn" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,3 +8,9 @@ variable "sns_topic_name" {
   type    = string
   default = "Message_Contactus_SNS"
 }
+
+# Define a list of email addresses
+variable "email_addresses" {
+  type    = list(string)
+  default = ["dainius.stepulevicius@t4connex.com","benjamin.wai@t4connex.com"]
+}
