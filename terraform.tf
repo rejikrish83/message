3b2944907671685b3d3ab9fba64f8686aed1bd40
rejikrish83 <rejikrish83@gmail.com>
2023-10-17 15:52:56 +0100

    updated ECR image path

--- terraform.tf
+++ terraform.tf
@@ -34,7 +34,7 @@ variable "az_count" {
 
 variable "app_image" {
   description = "Docker image to run in the ECS cluster"
-  default     = "470502905291.dkr.ecr.eu-north-1.amazonaws.com/message-app-repo:latest"
+  default     = "470502905291.dkr.ecr.eu-north-1.amazonaws.com/message-app:latest"
 }
 
 variable "app_port" {
