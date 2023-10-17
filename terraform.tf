bdd2096257a35587a7be32792aa4462c23632211
rejikrish83 <rejikrish83@gmail.com>
2023-10-17 21:20:59 +0100

    updated ECR application.properties file

--- terraform.tf
+++ terraform.tf
@@ -29,7 +29,7 @@ variable "ecs_task_execution_role_name" {
 
 variable "az_count" {
   description = "Number of AZs to cover in a given region"
-  default     = "2"
+  default     = "3"
 }
 
 variable "app_image" {
